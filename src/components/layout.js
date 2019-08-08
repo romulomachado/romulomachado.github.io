6592435554cdc701c619cccbb649218ae424677b
Rômulo Machado <romulo.machado5@gmail.com>
2019-08-08 14:12:28 -0300

    am I living or just updating gatsby

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,24 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import "./layout.css"
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
         }
       }
-    `}
-    render={data => (
-      <>
-        <main>{children}</main>
-      </>
-    )}
-  />
-)
+    }
+  `)
+
+  return (
+    <main>{children}</main>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
