7b47347e0592c6ad310e582297321fe32a5a2c85
Rômulo Machado <romulo.machado5@gmail.com>
2020-11-10 13:07:51 -0300

    Remove src/gatsby-plugin-theme-ui/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import React from "react"
+import React from 'react'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+  return <h1>Hello, World!</h1>
 }
+
+export default Home
