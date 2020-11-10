40a132bfabd64e867228b2d373baca1d606f623f
Rômulo Machado <romulo.machado5@gmail.com>
2020-11-10 13:12:44 -0300

    Revert "Remove src/gatsby-plugin-theme-ui/index.js"
    This reverts commit 7b47347e0592c6ad310e582297321fe32a5a2c85.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react"
 
-const Home = () => {
-  return <h1>Hello, World!</h1>
+export default function Home() {
+  return <div>Hello world!</div>
 }
-
-export default Home
