8412f52179e14783f2880ec92e87d185049e7dc2
Rômulo Machado <romulo.machado5@gmail.com>
2020-11-12 09:25:42 -0300

    Config gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,19 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: ['gatsby-plugin-theme-ui'],
+  plugins: [
+    'gatsby-plugin-theme-ui',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rômulo Machado`,
+        short_name: `Rômulo`,
+        start_url: `/`,
+        // background_color: `#f7f0eb`,
+        // theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+  ],
 }
