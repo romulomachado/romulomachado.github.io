62f740dfe43ef0b4bb99d280d2b49ce5dc89417e
Rômulo Machado <romulo.machado5@gmail.com>
2020-11-23 13:58:47 -0300

    I don't need this

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,12 +11,6 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Rômulo Machado`,
-        short_name: `Rômulo`,
-        start_url: `/`,
-        // background_color: `#f7f0eb`,
-        // theme_color: `#a2466c`,
-        display: `standalone`,
         icon: `static/favicon.png`,
       },
     },
